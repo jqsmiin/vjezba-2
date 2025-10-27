778780041d1a3863ee457417f94807379b39603b
jqsmiin <jasmin.oruc23@gmail.com>
2025-10-27 12:35:59 +0100

    Initial commit

---
+++ src/main/java/com/example/vjezba2/Vjezba2Application.java
@@ -0,0 +1,13 @@
+package com.example.vjezba2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vjezba2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vjezba2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vjezba2/Vjezba2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vjezba2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vjezba2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
